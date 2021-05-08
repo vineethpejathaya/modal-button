1b8e4e779cf9ebc0973090d8351dc1f61dbcb4eb
vineethpejathaya <vineethpejathaya@gmail.com>
2021-05-08 11:26:27 +0530

    code changes

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 
+const btnCloseModal = document.querySelector(".close-modal");
+const btnShowModal= document.querySelectorAll(".show-modal");
+
+
+function showModal(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function closeModal(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i=0; i<btnShowModal.length; i++)
+{
+    btnShowModal[i].addEventListener('click',showModal)
+}
+
+btnCloseModal.addEventListener('click',closeModal);
+overlay.addEventListener('click',closeModal)
